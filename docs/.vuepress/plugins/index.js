9a6f82ff4d33e265777d80fee577655e710d7382
lifachao <2390210930@qq.com>
2022-02-10 16:29:24 +0800

    :bug: fix:音乐播放器bug

--- docs/.vuepress/plugins/index.js
+++ docs/.vuepress/plugins/index.js
@@ -37,21 +37,36 @@ module.exports = {
       "meting",
       {
         meting: {
+          // 网易
           server: "netease",
+          // 读取歌单列表
           type: "playlist",
           mid: "7228385774",
-          // auto: 'https://y.qq.com/n/ryqq/player'
         },
+        // 不配置该项的话不会出现全局播放器
         aplayer: {
-          order: "random",
-          lrcType: 0,
+          // 吸底模式
+          fixed: true,
+          mini: true,
+          // 自动播放
+          autoplay: true,
+          // 歌曲栏折叠
           listFolded: true,
+          // 颜色
+          theme: "#f9bcdd",
+          // 播放顺序为随机
+          order: "random",
+          // 初始音量
+          volume: 0.3,
+          // 关闭歌词显示
+          lrcType: 0,
         },
         mobile: {
-          cover: true,
-          lrc: false,
+          // 手机端去掉cover图
+          cover: false,
         },
       },
     ],
+
   ],
 };
